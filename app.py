40d2d7c70c1fb333d2c275a3f60c673e7e118aed
girishlohar <girishlohar2006@gmail.com>
2026-05-08 09:35:20 +0530

    IntelX OSINT final version

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify, send_file, redirect, session
 import requests
 import dns.resolver
@@ -577,11 +578,6 @@ def report():
 
 
 # ================= MAIN =================
-
 if __name__ == "__main__":
-
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
